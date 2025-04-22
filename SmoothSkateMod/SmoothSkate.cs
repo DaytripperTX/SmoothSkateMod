4f11806b8d76e3d3dd22f04e02da86659eb244e7
DaytripperTX <chad98tx@gmail.com>
2025-04-22 01:50:28 -0500

    Add project files.

---
+++ SmoothSkateMod/SmoothSkate.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(SmoothSkateMod.SmoothSkate), "SmoothSkate", "1.0.0", "volcomtx")]
+[assembly: MelonGame("TVGS", "Schedule One")]
+
+namespace SmoothSkateMod
+{
+    public class SmoothSkate : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HarmonyInstance.PatchAll();
+            MelonLogger.Msg("SmoothSkate loaded. Ride smooth!");
+        }
+    }
+}
