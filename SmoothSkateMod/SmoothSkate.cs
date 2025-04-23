2e84be58b28bcb3369fdd8866a6c69b873f96daf
DaytripperTX <chad98tx@gmail.com>
2025-04-22 21:56:29 -0500

    set references for il2cpp

--- SmoothSkateMod/SmoothSkate.cs
+++ SmoothSkateMod/SmoothSkate.cs
@@ -1,6 +1,7 @@
 ﻿using MelonLoader;
 using HarmonyLib;
 using UnityEngine;
+using Il2CppScheduleOne.Skating;
 
 [assembly: MelonInfo(typeof(SmoothSkateMod.SmoothSkate), "SmoothSkate", "0.1.0", "volcomtx")]
 [assembly: MelonGame("TVGS", "Schedule I")]
@@ -13,15 +14,21 @@ namespace SmoothSkateMod
         {
             HarmonyInstance.PatchAll();
             MelonLogger.Msg("SmoothSkate loaded. Ride smooth!");
-        }
-    }
 
-    [HarmonyPatch(typeof(ScheduleOne.Skating.Skateboard), "GetSurfaceSmoothness")]
+            foreach (var method in typeof(Skateboard).GetMethods())
+            {
+                MelonLogger.Msg("Found method: " + method.Name);
+            }
+        }
+}
+
+    [HarmonyPatch(typeof(Skateboard), "GetSurfaceSmoothness")]
     public static class Skateboard_GetSurfaceSmoothness_Patch
     {
         static bool Prefix(ref float __result)
         {
             __result = 1f;
+            MelonLogger.Msg("setting smoothness to 1f");
             return false; // Skip original method
         }
     }
